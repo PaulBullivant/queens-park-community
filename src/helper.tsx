9041321905e385b062bb90aef81afbae511ce159
Paul Bullivant <pdbullivant@hotmail.com>
2024-04-30 19:19:47 +0100

    Update helper

--- src/helper.tsx
+++ src/helper.tsx
@@ -3,8 +3,8 @@ export class Urls {
     public static  SignUp = 'https://docs.google.com/forms/d/e/1FAIpQLScJpHQ2sVjTQyvx_ysCsuBIykouZsfMumQlKgzbOa8Zuoh3LA/viewform?usp=sf_link';
     public static  GitHub = 'https://github.com/PaulBullivant/queens-park-community';
     public static  Code = 'code';
+    public static  HowItWorks = 'code';
     public static  Privacy = 'privacy';
-    public static  HowItWorks = 'how-it-works';
     public static  WhatsAppCommunities = 'https://faq.whatsapp.com/495856382464992';
     public static WhatsApp = 'https://www.whatsapp.com/';
 }
